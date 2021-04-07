8466befeaf700a44cdcec2eab2b2a9488d0c7061
hamza027riaz <hamzariaz803@gmail.com>
2021-04-07 10:46:07 +0500

    Add assests and UI.dart
    Add assets folder of font and images.
    and add UI.dart

---
+++ lib/UI.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class UI extends StatefulWidget {
+  @override
+  _UI createState() => _UI();
+}
+
+class _UI extends State<UI>
+    with SingleTickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+  }
+
+
+}
